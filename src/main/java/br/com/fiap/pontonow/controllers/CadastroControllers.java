bf0de960846c9bd00e39609f5e22fccd813ee5b9
wesleykleim <wesleykleim@gmail.com>
2023-03-27 23:16:03 -0300

    CRUD Basico

--- src/main/java/br/com/fiap/pontonow/controllers/CadastroControllers.java
+++ src/main/java/br/com/fiap/pontonow/controllers/CadastroControllers.java
@@ -6,13 +6,18 @@ import java.util.List;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import br.com.fiap.pontonow.models.Cadastro;
 import br.com.fiap.pontonow.repository.CadastroRepository;
+
+
 
 @Controller
 public class CadastroControllers {
@@ -24,10 +29,8 @@ public class CadastroControllers {
 
     
     @GetMapping("/api/cadastro")
-   
     public  List<Cadastro> index(){
-        
-         return cadastros; 
+         return repository.findAll(); 
         
     }
 
@@ -35,5 +38,44 @@ public class CadastroControllers {
     public ResponseEntity <Cadastro> create(@RequestBody Cadastro cadastro){
         log.info("Acesso do usuario" + cadastro);
         repository.save(cadastro);
+        return ResponseEntity.status(HttpStatus.CREATED).body(cadastro);
     }
+    /**
+     * @param id
+     * @return
+     */
+
+    @GetMapping(value="/api/cadastro/{id}")
+    public ResponseEntity<Cadastro> show(@PathVariable Long id) {
+        log.info("Buscando Cadastro com id " + id);
+        var cadastroEncontrado = repository.findById(id);
+
+        if(cadastroEncontrado.isEmpty())
+
+            return ResponseEntity.notFound().build();
+
+        return ResponseEntity.ok(cadastroEncontrado.get());
+
+    }
+
+    /**
+     * @param id
+     * @return
+     */
+    @DeleteMapping("/api/cadastro/{id}")
+    public ResponseEntity<Cadastro> destroy(@PathVariable Long id){
+        log.info("Apagando Cadastro com id " + id);
+        var cadastroEncontrado = repository.findById(id);
+
+        if (cadastroEncontrado.isEmpty())
+
+            return ResponseEntity.notFound().build();
+
+        repository.delete(cadastroEncontrado.get());
+
+        return ResponseEntity.noContent().build();
+        
+    }
+
+    
 }
--- src/main/java/br/com/fiap/pontonow/repository/CadastroRepository.java
+++ src/main/java/br/com/fiap/pontonow/repository/CadastroRepository.java
@@ -1,9 +1,13 @@
 package br.com.fiap.pontonow.repository;
 
+import java.time.LocalDate;
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import br.com.fiap.pontonow.models.Cadastro;
 
 public interface CadastroRepository extends JpaRepository<Cadastro, Long>{
+    List<Cadastro>findByData(LocalDate data);
     
 }
