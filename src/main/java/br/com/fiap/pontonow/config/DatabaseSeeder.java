92699ad7c820c5dc9337587cf7dabde7c8ebde0c
wesleykleim <wesleykleim@gmail.com>
2023-04-10 21:43:23 -0300

    relacionamentos

--- src/main/java/br/com/fiap/pontonow/config/DatabaseSeeder.java
+++ src/main/java/br/com/fiap/pontonow/config/DatabaseSeeder.java
@@ -1,8 +1,13 @@
 package br.com.fiap.pontonow.config;
 
+import java.util.Date;
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Configuration;
+
+import br.com.fiap.pontonow.models.Acesso;
 
 @Configuration
 public class DatabaseSeeder implements CommandLineRunner {
@@ -12,7 +17,11 @@ public class DatabaseSeeder implements CommandLineRunner {
 
     @Override
     public void run(String... args) throws Exception {
-        acessoRepository.save()
+        acessoRepository.saveAll(List.of(
+            new Acesso (1L, "wesleykleim@gmail.com" ,new String(30), "login");
+            new Acesso( 2L, "@#$W05050505" ,new String(8), "senha");
+            new Acesso(3L, "10/04/2023", new Date(), "data");
+        ));
     }
     
 }
